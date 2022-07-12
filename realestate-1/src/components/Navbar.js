158c623e84fc726c836775406ee7eb367bd5fa8d
Matthew Charles <matthewcharles90@gmail.com>
2022-07-11 23:14:45 -0400

    Update NavJS
    Nav-menu  has been added.

--- realestate-1/src/components/Navbar.js
+++ realestate-1/src/components/Navbar.js
@@ -11,6 +11,12 @@ const Navbar = () => {
         <div className='container'>
           <h1><span><BsFillHouseFill />Real</span>Estate</h1>
           <button className='btn'>Sign In</button>
+          <ul className='nav-menu'>
+             <li><a href='#'>Home</a></li>
+             <li><a href='#'>Search</a></li>
+             <li><a href='#'>About</a></li>
+             <li><a href='#'>Contact</a></li>
+          </ul>
         </div>
     </div>
   )
