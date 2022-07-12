70caa02e5844681d61ca58e2af51401072ebe0af
Matthew Charles <matthewcharles90@gmail.com>
2022-07-11 20:11:39 -0400

    icons
    added icons to Navbarjs

--- realestate-1/src/components/Navbar.js
+++ realestate-1/src/components/Navbar.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Navbar.css'
+import {HiOutlineMenuAlt4} from 'react-icons/hi'
+import {FaRegTimesCircle} from 'react-icons/fa'
+import {BsFillHouseFill} from 'react-icons/bs'
 
 const Navbar = () => {
   return (
