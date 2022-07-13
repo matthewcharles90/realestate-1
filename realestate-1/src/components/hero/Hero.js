524aa514e8b6e37c7992fb2b8da8366e9645e5cf
Matthew Charles <matthewcharles90@gmail.com>
2022-07-13 15:55:58 -0400

    Hero Update
    Form and P tag created

--- realestate-1/src/components/hero/Hero.js
+++ realestate-1/src/components/hero/Hero.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+        <div className='content'>
+            <h1>Find comfort with us</h1>
+            <p className='search-text'> Search the larget selection of comfortable and luxury apartments. </p>
+            <form className='search'>
+                <div>
+                    <input type='text' placeholder='Enter Keyword..' />
+                </div>
+            </form>
+        </div>
+    </div>
   )
 }
 
