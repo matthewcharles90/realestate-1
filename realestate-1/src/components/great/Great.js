ea1d672b59a16e38a173beff20cbc4cf6943a4ac
Matthew Charles <matthewcharles90@gmail.com>
2022-07-14 23:59:32 -0400

    Update Assets
    images of apt 1, 2, 3

--- realestate-1/src/components/great/Great.js
+++ realestate-1/src/components/great/Great.js
@@ -7,7 +7,16 @@ const Great = () => {
         <h1>Find Great Rated Properties</h1>
         <div>
             <p><span className='bold'>All</span></p>
+            <p>Commercial</p>
+            <p>Residentual</p>
+            <p>Vacation</p>
         </div>
+        <div className='container'>
+            <img src={} alt='' />
+            <img src={} alt='' />
+            <img src={} alt='' />
+        </div>
+        <button className='btn'>View All</button>
     </div>
   )
 }
