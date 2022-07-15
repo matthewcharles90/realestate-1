30afc64e735de38aa87c46c4a6660a782c6503ad
Matthew Charles <matthewcharles90@gmail.com>
2022-07-15 00:19:39 -0400

    Update Appjs
    import great component

--- realestate-1/src/App.js
+++ realestate-1/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero';
+import Great from './components/great/Great';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Great />
   
     </>
   );
