adb91120e93a8156df3014427ab21f4127af2aad
Matthew Charles <matthewcharles90@gmail.com>
2022-07-15 14:27:16 -0400

    Update AppJS in Main Branch
    imported Footer

--- realestate-1/src/App.js
+++ realestate-1/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero';
 import Great from './components/great/Great';
 import Featured from './components/featured/Featured'
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Great />
       <Featured />
+      <Footer />
   
     </>
   );
